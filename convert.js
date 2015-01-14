9f132e182298a13c2e2075b28291e78dee04643e
Andrew G Burkus <da8urk@gmail.com>
2015-01-13 21:33:12 -0500

    updates

--- convert.js
+++ convert.js
@@ -1,28 +1,32 @@
 
-	var avogrado = function() {
-		return 6.02 * Math.pow(10, 23);
-	};
+var avogrado = function() {
+	return 6.02 * Math.pow(10, 23);
+};
 
-	var toLiters = function(x) {
-		return x * 3.79;
-	};
+var gallonsToLiters = function(x) {
+	return x * 3.79;
+};
 
-	var toGalons = function(x) {
-		return x / 3.79;
-	};
+var litersToGalons = function(x) {
+	return x / 3.79;
+};
 
-	var toGrams = function(x) {
-		return x * 454.0;
-	};
+var poundsToGrams = function(x) {
+	return x * 454.0;
+};
 
-	var toPounds = function(x) {
-		return x / 454.0;
-	};
+var gramsToPounds = function(x) {
+	return x / 454.0;
+};
 
-	var molesToParticles = function(x) {
-		return x * avogrado();
-	};
+var molesToParticles = function(x) {
+	return x * avogrado();
+};
 
-	var particlesToMoles = function(x) {
-		return x / avogrado();
-	};
+var particlesToMoles = function(x) {
+	return x / avogrado();
+};
+
+var molesToLiters = function(x) {
+	return x * 22.4;
+};
